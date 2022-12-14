4eb4e9898a94add48cb82e9fc2474ad244d04f2f
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-14 12:06:03 +0300

    bot update

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
 client.run(TOKEN)
