749f75a1212ba0785bc176445c4007807c0b3a84
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-14 11:31:34 +0300

    bot update

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 # Import Libraries
-from config import Schema, log_functions
 from config.Config import APP_Settings
-from kafka import KafkaConsumer, KafkaProducer
-from json import dumps
-import json
-
-# Discord Libraries
 import discord
-from discord.ext import commands
+from dotenv import load_dotenv
+
+# Load ENV
 TOKEN = APP_Settings.DISCORD_TOKEN
 
 # Initialize Bot and Denote The Command Prefix
@@ -15,38 +11,6 @@ client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-
-	channel = client.get_channel(1051844419105607781)
-
-	# Kafka Consumer
-	Kafka_Discord_Consumer = KafkaConsumer('RAW.Discord', bootstrap_servers="165.227.154.147:9092", group_id="Data_Consumer", auto_offset_reset='earliest', enable_auto_commit=False)
-
-	try:
-
-		for Message in Kafka_Discord_Consumer:
-
-			# handle Message.
-			#Kafka_Message = Schema.IoT_Data_Pack_Model(**json.loads(Message.value.decode()))
-
-			# Handle Headers
-			Command = Message.headers[0][1].decode('ASCII')
-			Device_ID = Message.headers[1][1].decode('ASCII')
-			Device_Time = Message.headers[2][1].decode('ASCII')
-			Device_IP = Message.headers[3][1].decode('ASCII')
-
-			Message = str(Device_ID) + " kodlu cihazdan veri geldi. \nKomut : " + str(Command) + " (" + str(Device_Time) + ") \n```JSON\n" + str(Message.value.decode()) + "```"
-
-			await channel.send(Message)
-
-			# Commit Message
-			Kafka_Discord_Consumer.commit()
-
-			# Log
-			print("Message send to discord. \n", Message)
-
-	finally:
-		
-		print("Error Accured !!")
+    print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
-
