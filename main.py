6a623fb6fa642dc22668da288ceab93e494fc4bb
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-14 12:01:40 +0300

    bot update

--- main.py
+++ main.py
@@ -5,21 +5,14 @@ from dotenv import load_dotenv
 
 # Load ENV
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+TOKEN = str(os.getenv('DISCORD_TOKEN'))
+GUILD = str(os.getenv('DISCORD_GUILD'))
 
 # Initialize Bot and Denote The Command Prefix
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'{client.user} has connected to Discord!')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
-client.run(str(TOKEN))
+client.run(TOKEN)
