aec174ed48600a1c3363bd7891a31b2c98e23803
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-14 12:08:40 +0300

    bot update

--- main.py
+++ main.py
@@ -12,17 +12,8 @@ GUILD = str(os.getenv('DISCORD_GUILD'))
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
-    )
-
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+async def on_message(message):
+    if 'happy birthday' in message.content.lower():
+        await message.channel.send('Happy Birthday! 🎈🎉')
 
 client.run(TOKEN)
