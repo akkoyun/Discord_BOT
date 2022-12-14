d4ab40879a4b0a515881adee7903ad215fa08543
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-14 12:15:06 +0300

    bot update

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ TOKEN = str(os.getenv('DISCORD_TOKEN'))
 GUILD = str(os.getenv('DISCORD_GUILD'))
 
 # Initialize Bot and Denote The Command Prefix
-#client = discord.Client(intents=discord.Intents.default())
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
+#client = discord.Client()
 
 @client.event
 async def on_ready():
