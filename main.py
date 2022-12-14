252a75603adc1f458e517057a32624c954087920
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-14 12:36:19 +0300

    bot update

--- main.py
+++ main.py
@@ -29,17 +29,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
     if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
+        await message.channel.send("STF")
 
 client.run(TOKEN)
